e2e1dabda038ada058dd9c5e50b8a9eed814e5f5
kipathipaka <kirankumar@bpatech.com>
2015-12-08 14:30:44 +0530

    Modified reciever to handle  , on device reboot setting alarm for location update

--- app/src/main/java/com/bpatech/trucktracking/Service/UpdateLocationReceiver.java
+++ app/src/main/java/com/bpatech/trucktracking/Service/UpdateLocationReceiver.java
@@ -1,6 +1,8 @@
 
         package com.bpatech.trucktracking.Service;
 
+        import android.app.AlarmManager;
+        import android.app.PendingIntent;
         import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,6 +17,19 @@ public class UpdateLocationReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(final Context context, Intent intent) {
          //Toast.makeText(context, "Reciverrrrrrrrrrr: ", Toast.LENGTH_SHORT).show();
+        System.out.println("inside receiver **************************");
+
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+            // Set the alarm here.
+            System.out.println("inside receiver boot completed condition**************************");
+            Toast.makeText(context, "Reciverrrrrrrrrrr:setting alarm on reboot ", Toast.LENGTH_LONG).show();
+            AlarmManager alarmManager=(AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent intentR = new Intent( context, UpdateLocationReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentR, 0);
+
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+10 * 60 * 1000,20 * 60 * 1000,
+                    pendingIntent);
+        }
 
         context.startService(new Intent(context, UpdateLocationService.class));
 
