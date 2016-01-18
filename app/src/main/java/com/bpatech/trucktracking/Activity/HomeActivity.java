f6c913211abfcedd3d08ffe4368ba99bfbcba4c6
Yugandhar <yugandhar@bpatech.com>
2016-01-18 12:55:10 +0530

    Changes made in HomeActivity regarding if location should get worked for both  highAccuracy and batterySaving

--- app/src/main/java/com/bpatech/trucktracking/Activity/HomeActivity.java
+++ app/src/main/java/com/bpatech/trucktracking/Activity/HomeActivity.java
@@ -10,7 +10,6 @@ import android.content.IntentSender;
 import android.content.res.Configuration;
 import android.location.Location;
 import android.os.AsyncTask;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
@@ -52,8 +51,6 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import timber.log.Timber;
 
 public class HomeActivity extends FragmentActivity  implements GoogleApiClient.ConnectionCallbacks,
 		GoogleApiClient.OnConnectionFailedListener {
@@ -103,7 +100,8 @@ public class HomeActivity extends FragmentActivity  implements GoogleApiClient.C
 			googleApiClient.connect();
 
 			locationRequest = LocationRequest.create();
-			locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 			locationRequest.setInterval(30 * 1000);
 			locationRequest.setFastestInterval(5 * 1000);
 			new GetandStroeMessages().execute("", "", "");
@@ -351,6 +349,7 @@ public void Enable_location_popup()
 	if (googleApiClient != null) {
 		LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
 				.addLocationRequest(locationRequest);
+
 
 		// **************************
 		builder.setAlwaysShow(true); // this is the key ingredient
